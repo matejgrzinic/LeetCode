eb52794e0a35f140b6e1342622f66091eb3d64c2
Matej Grzinic <grzinic.matej@gmail.com>
2020-03-18 10:35:18 +0100

    renaming variables

--- Python/5. Longest Palindromic Substring.py
+++ Python/5. Longest Palindromic Substring.py
@@ -4,44 +4,39 @@ class Solution:
         n = len(s)
         if n == 1:
             return s[0]
-        for i in range(n - 1):
-            p = s[i]
-            
-            start, end = i, i            
-            for k in range(1, n):
-                if i + k < n and s[i] == s[i +k]:
-                    p += s[i]
+        end_of_loop = n - 1
+        for char_index in range(end_of_loop):
+            string = s[char_index]            
+            start, end = char_index, char_index            
+            for offset in range(1, n):
+                if char_index + offset < n and s[char_index] == s[char_index + offset]:
+                    string += s[char_index]
                     end += 1
-                    if i - k >= 0 and s[i] == s[i - k]:
-                        p += s[i]
+                    if char_index - offset >= 0 and s[char_index] == s[char_index - offset]:
+                        string += s[char_index]
                         start -= 1
                     else:
                         break   
                 else:
-                    break                                    
-            
-            if len(p) > len(best):
-                best = p               
-                       
+                    break                                                  
         
             for index in range(1, n):
-                si = start - index
-                ei = end + index
-                if si >= 0 and ei < n and s[si] == s[ei]:
-                    p = s[si] + p + s[si]
-                    if len(p) > len(best):
-                        best = p 
+                start_index = start - index
+                end_index = end + index
+                if start_index >= 0 and end_index < n and s[start_index] == s[end_index]:
+                    string = s[start_index] + string + s[start_index]
+                    if len(string) > len(best):
+                        best = string 
                 else:
                     break                   
-           
+            
+            if len(string) > len(best):
+                best = string  
+                end_of_loop = n - len(best)
             
         return best
     
-    def isPalindrome(self, s):
-        for index in range(len(s) // 2):
-            if s[index] != s[-index - 1]:
-                return False
-        return True
+    
         
 s = Solution()
 print(s.longestPalindrome("caacc"))
