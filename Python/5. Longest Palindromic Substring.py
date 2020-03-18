2724eabd0c5271057cd11ab57cc458d9f734c5f6
Matej Grzinic <grzinic.matej@gmail.com>
2020-03-18 10:16:07 +0100

    first linear solution for 5.

--- Python/5. Longest Palindromic Substring.py
+++ Python/5. Longest Palindromic Substring.py
@@ -1,15 +1,40 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if s == "":
-            return s
-        results = []
-        best = s[0]
-        for char in s:
-            for index, substring in enumerate(results):
-                results[index] += char
-                if self.isPalindrome(results[index]) and len(results[index]) > len(best):
-                    best = results[index]
-            results.append(char)    
+        best = ""
+        n = len(s)
+        if n == 1:
+            return s[0]
+        for i in range(n - 1):
+            p = s[i]
+            
+            start, end = i, i            
+            for k in range(1, n):
+                if i + k < n and s[i] == s[i +k]:
+                    p += s[i]
+                    end += 1
+                    if i - k >= 0 and s[i] == s[i - k]:
+                        p += s[i]
+                        start -= 1
+                    else:
+                        break   
+                else:
+                    break                                    
+            
+            if len(p) > len(best):
+                best = p               
+                       
+        
+            for index in range(1, n):
+                si = start - index
+                ei = end + index
+                if si >= 0 and ei < n and s[si] == s[ei]:
+                    p = s[si] + p + s[si]
+                    if len(p) > len(best):
+                        best = p 
+                else:
+                    break                   
+           
+            
         return best
     
     def isPalindrome(self, s):
@@ -19,8 +44,8 @@ class Solution:
         return True
         
 s = Solution()
-print(s.longestPalindrome("babad"))
+print(s.longestPalindrome("caacc"))
 # print("Expected: bab|aba, got:", s.longestPalindrome("babad"))
-# print("Expected: bb, got:", s.longestPalindrome("cbbd"))
-# print(s.longestPalindrome("vmqjjfnxtyciixhceqyvibhdmivndvxyzzamcrtpywczjmvlodtqbpjayfchpisbiycczpgjdzezzprfyfwiujqbcubohvvyakxfmsyqkysbigwcslofikvurcbjxrccasvyflhwkzlrqowyijfxacvirmyuhtobbpadxvngydlyzudvnyrgnipnpztdyqledweguchivlwfctafeavejkqyxvfqsigjwodxoqeabnhfhuwzgqarehgmhgisqetrhuszoklbywqrtauvsinumhnrmfkbxffkijrbeefjmipocoeddjuemvqqjpzktxecolwzgpdseshzztnvljbntrbkealeemgkapikyleontpwmoltfwfnrtnxcwmvshepsahffekaemmeklzrpmjxjpwqhihkgvnqhysptomfeqsikvnyhnujcgokfddwsqjmqgsqwsggwhxyinfspgukkfowoxaxosmmogxephzhhy"))
+#print("Expected: bb, got:", s.longestPalindrome("cbbd"))
+#print(s.longestPalindrome("vmqjjfnxtyciixhceqyvibhdmivndvxyzzamcrtpywczjmvlodtqbpjayfchpisbiycczpgjdzezzprfyfwiujqbcubohvvyakxfmsyqkysbigwcslofikvurcbjxrccasvyflhwkzlrqowyijfxacvirmyuhtobbpadxvngydlyzudvnyrgnipnpztdyqledweguchivlwfctafeavejkqyxvfqsigjwodxoqeabnhfhuwzgqarehgmhgisqetrhuszoklbywqrtauvsinumhnrmfkbxffkijrbeefjmipocoeddjuemvqqjpzktxecolwzgpdseshzztnvljbntrbkealeemgkapikyleontpwmoltfwfnrtnxcwmvshepsahffekaemmeklzrpmjxjpwqhihkgvnqhysptomfeqsikvnyhnujcgokfddwsqjmqgsqwsggwhxyinfspgukkfowoxaxosmmogxephzhhy"))
 
