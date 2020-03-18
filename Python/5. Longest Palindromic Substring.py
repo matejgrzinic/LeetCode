59bb84069481eebfdc07b4c2bb5a5cda949a778a
Matej Grzinic <grzinic.matej@gmail.com>
2020-03-18 10:41:46 +0100

    very optimal and very intuitive solution

--- Python/5. Longest Palindromic Substring.py
+++ Python/5. Longest Palindromic Substring.py
@@ -1,40 +1,21 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        best = ""
+        if len(s) < 2 or s ==s[::-1]:
+            return s
         n = len(s)
-        if n == 1:
-            return s[0]
-        end_of_loop = n - 1
-        for char_index in range(end_of_loop):
-            string = s[char_index]            
-            start, end = char_index, char_index            
-            for offset in range(1, n):
-                if char_index + offset < n and s[char_index] == s[char_index + offset]:
-                    string += s[char_index]
-                    end += 1
-                    if char_index - offset >= 0 and s[char_index] == s[char_index - offset]:
-                        string += s[char_index]
-                        start -= 1
-                    else:
-                        break   
-                else:
-                    break                                                  
+        start, maxlen = 0,1
         
-            for index in range(1, n):
-                start_index = start - index
-                end_index = end + index
-                if start_index >= 0 and end_index < n and s[start_index] == s[end_index]:
-                    string = s[start_index] + string + s[start_index]
-                    if len(string) > len(best):
-                        best = string 
-                else:
-                    break                   
+        for i in range(n):
+            odd = s[i-maxlen-1:i+1]
+            even = s[i-maxlen:i+1]
             
-            if len(string) > len(best):
-                best = string  
-                end_of_loop = n - len(best)
-            
-        return best
+            if i - maxlen -1>=0 and odd == odd[::-1]:
+                start = i - maxlen - 1
+                maxlen +=2                
+            elif i - maxlen >=0 and even ==even[::-1]:
+                start = i - maxlen 
+                maxlen +=1
+        return s[start:start+maxlen]
     
     
         
