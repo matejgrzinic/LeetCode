39a65f959218566d2ca18f2459fc7a4e9fdada43
Matej Grzinic <grzinic.matej@gmail.com>
2020-03-19 08:41:07 +0100

    adding 6.

---
+++ Python/6. ZigZag Conversion.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/zigzag-conversion/
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        # Runtime: 36 ms, faster than 99.98% of Python3 online submissions for ZigZag Conversion.
+        # Memory Usage: 13 MB, less than 98.57% of Python3 online submissions for ZigZag Conversion        
+        
+        if numRows == 1:
+            return s
+        else:           
+            rows = [''] * numRows
+
+            going_up = True
+            index_row = 0        
+
+            for char in s:
+                rows[index_row] += char           
+                index_row = index_row + 1 if going_up else index_row - 1
+                if index_row == 0 or index_row == numRows - 1:
+                    going_up = not going_up                   
+            return "".join(rows)
+        
+
+s = Solution()
+print(s.convert("AB", 2))
+
+
