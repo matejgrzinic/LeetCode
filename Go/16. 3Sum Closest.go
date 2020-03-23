4d0cd2e3e3a634efc43dd171bc7dafaa662f774b
Matej Grzinic <grzinic.matej@gmail.com>
2020-03-23 08:03:38 +0100

    solving 16 in go

---
+++ Go/16. 3Sum Closest.go
@@ -0,0 +1,59 @@
+// https://leetcode.com/problems/3sum-closest/submissions/
+
+// Runtime: 4 ms, faster than 96.48% of Go online submissions for 3Sum Closest.
+// Memory Usage: 2.7 MB, less than 50.00% of Go online submissions for 3Sum Closest.
+
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+)
+
+func main() {
+	fmt.Println(threeSumClosest([]int{-1, 2, 1, -4}, 1))    // 2
+	fmt.Println(threeSumClosest([]int{1, 1, 1, 0}, 100))    // 3
+	fmt.Println(threeSumClosest([]int{1, 1, -1, -1, 3}, 3)) // 3
+}
+
+func threeSumClosest(nums []int, target int) int {
+	sort.Ints(nums)
+	best := math.MaxInt32
+	bestsum := math.MaxInt32
+
+	for index, num := range nums {
+		l := index + 1
+		r := len(nums) - 1
+
+		if index > 0 && nums[index] == nums[index-1] {
+			continue
+		}
+
+		if index > len(nums)-3 {
+			return bestsum
+		}
+
+		for l < r {
+			sum := num + nums[l] + nums[r]
+			diff := intAbs(sum - target)
+			if diff < best {
+				best = diff
+				bestsum = sum
+			} else if sum > target {
+				r--
+			} else {
+				l++
+			}
+		}
+
+	}
+	return bestsum
+}
+
+func intAbs(num int) int {
+	if num < 0 {
+		num *= -1
+	}
+	return num
+}
