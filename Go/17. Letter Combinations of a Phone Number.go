cbdbb96ce146e92f15980c0b54df1414c5ce355d
Matej Grzinic <grzinic.matej@gmail.com>
2020-03-23 09:36:09 +0100

    adding additional go solution to 17

--- Go/17. Letter Combinations of a Phone Number.go
+++ Go/17. Letter Combinations of a Phone Number.go
@@ -33,3 +33,33 @@ func addLetter(current string, next string, keymap map[string][]string, result *
 		}
 	}
 }
+
+func letterCombinationsSecond(digits string) []string {
+
+	// Runtime: 0 ms, faster than 100.00% of Go online submissions for Letter Combinations of a Phone Number.
+	// Memory Usage: 2.3 MB, less than 100.00% of Go online submissions for Letter Combinations of a Phone Number.
+
+	result := []string{}
+
+	keymap := map[string][]string{"2": []string{"a", "b", "c"}, "3": []string{"d", "e", "f"},
+		"4": []string{"g", "h", "i"}, "5": []string{"j", "k", "l"}, "6": []string{"m", "n", "o"},
+		"7": []string{"p", "q", "r", "s"}, "8": []string{"t", "u", "v"}, "9": []string{"w", "x", "y", "z"}}
+
+	if len(digits) == 0 {
+		return result
+	}
+
+	if len(digits) == 1 {
+		return keymap[string(digits[0])]
+	}
+
+	for _, key := range keymap[string(digits[0])] {
+		r2 := letterCombinations(digits[1:])
+		for index := range r2 {
+			r2[index] = string(key) + r2[index]
+			result = append(result, r2[index])
+		}
+	}
+
+	return result
+}
