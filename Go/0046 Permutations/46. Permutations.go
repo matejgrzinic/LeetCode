a32069666a4fb382bf4bf2a3ca19b71969de6f50
Matej Grzinic <grzinic.matej@gmail.com>
2020-04-11 08:49:05 +0200

    solving #46

---
+++ Go/0046 Permutations/46. Permutations.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"sort"
+
+	"./test0046"
+)
+
+func main() {
+	test0046.Test(permute)
+}
+
+func permute(nums []int) [][]int {
+	result := [][]int{}
+	sort.Ints(nums)
+	copyAppend(&result, &nums)
+	for {
+		if nextPermutation(nums) {
+			copyAppend(&result, &nums)
+		} else {
+			break
+		}
+	}
+	return result
+}
+
+func copyAppend(result *[][]int, nums *[]int) {
+	copyarray := make([]int, len(*nums))
+	copy(copyarray, (*nums)[:])
+	*result = append(*result, copyarray)
+}
+
+func nextPermutation(nums []int) bool {
+	x := -1
+
+	for i := 1; i < len(nums); i++ {
+		if nums[i-1] < nums[i] {
+			x = i - 1
+		}
+	}
+	if x == -1 {
+		return false
+	}
+
+	y := -1
+	for i := 0; i < len(nums); i++ {
+		if nums[x] < nums[i] {
+			y = i
+		}
+	}
+	nums[x], nums[y] = nums[y], nums[x]
+
+	for i, j := x+1, len(nums)-1; i < j; i, j = i+1, j-1 {
+		nums[i], nums[j] = nums[j], nums[i]
+	}
+
+	return true
+}
---
+++ Go/0046 Permutations/readme.md
@@ -0,0 +1,24 @@
+# 46. Permutations
+
+https://leetcode.com/problems/permutations/
+
+```
+Runtime: 0 ms, faster than 100.00% of Go online submissions for Permutations.
+Memory Usage: 2.6 MB, less than 100.00% of Go online submissions for Permutations.
+```
+
+Given a collection of **distinct** integers, return all possible permutations.
+
+**Example:**
+```
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+```
