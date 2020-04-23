3d31a6eb7c6c644c50aec670d2a05b2088ac7834
Matej Grzinic <grzinic.matej@gmail.com>
2020-04-23 09:00:15 +0200

    making #51 code better to read

--- Go/0051 N-Queens/51. N-Queens.go
+++ Go/0051 N-Queens/51. N-Queens.go
@@ -11,50 +11,50 @@ func main() {
 func solveNQueens(n int) [][]string {
 	result := [][]string{}
 	for i := 0; i < n; i++ {
-		helper([]int{i}, n, &result)
+		result = append(result, helper([]int{i}, n)...)
 	}
 	return result
 }
 
-func helper(positions []int, n int, result *[][]string) {
+func helper(positions []int, n int) [][]string {
+	result := [][]string{}
 	if len(positions) == n {
-		solution := []string{}
-		for _, i := range positions {
-			str := ""
-			for j := 0; j < i; j++ {
-				str += "."
-			}
-			str += "Q"
-			for j := i + 1; j < n; j++ {
-				str += "."
-			}
-			solution = append(solution, str)
-		}
-		*result = append(*result, solution)
-		return
+		result = append(result, buildStringResult(positions))
+		return result
 	}
-	newY := len(positions)
+
 	for i := 0; i < n; i++ {
 		isValid := true
-
 		for j := 0; j < len(positions); j++ {
-			if positions[j] == i {
-				isValid = false
-				break
-			}
-			if isDiagonal(j, positions[j], newY, i) {
+			if positions[j] == i || isDiagonal(j, positions[j], len(positions), i) {
 				isValid = false
 				break
 			}
 		}
-
 		if isValid {
 			copyarray := make([]int, len(positions))
 			copy(copyarray, positions)
 			copyarray = append(copyarray, i)
-			helper(copyarray, n, result)
+			result = append(result, helper(copyarray, n)...)
 		}
 	}
+	return result
+}
+
+func buildStringResult(positions []int) []string {
+	solution := []string{}
+	for _, i := range positions {
+		str := ""
+		for j := 0; j < i; j++ {
+			str += "."
+		}
+		str += "Q"
+		for j := i + 1; j < len(positions); j++ {
+			str += "."
+		}
+		solution = append(solution, str)
+	}
+	return solution
 }
 
 func isDiagonal(y1 int, x1 int, y2 int, x2 int) bool {
