0c7c990ea6be731b33431782d91fa3fafccf1afe
Matej Grzinic <grzinic.matej@gmail.com>
2020-05-11 08:56:25 +0200

    solving #61

---
+++ Go/0061 Rotate List/61. Rotate List.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	. "./test0061"
+)
+
+func main() {
+	Test(rotateRight)
+}
+
+func rotateRight(head *ListNode, k int) *ListNode {
+	if k == 0 {
+		return head
+	}
+	fast := head
+	size := 0
+	ok := true
+	for i := 0; i < k; i++ {
+		if fast != nil {
+			fast = fast.Next
+			size++
+		} else {
+			ok = false
+			break
+		}
+	}
+	if fast == nil && size == k || size == 0 {
+		return head
+	}
+	if ok {
+		slow := head
+		for fast.Next != nil {
+			fast = fast.Next
+			slow = slow.Next
+			size++
+		}
+		fast.Next = head
+		head = slow.Next
+		slow.Next = nil
+	} else {
+		head = rotateRight(head, k%size)
+	}
+	return head
+}
