86a3d04419bbb16cabfc382dac414f26289916d0
Grzinic Matej <grzinic.matej@gmail.com>
2022-07-16 01:00:55 +0200

    First c# leetcode

---
+++ C sharp/0258 Add Digits/Program.cs
@@ -0,0 +1,29 @@
+﻿// https://leetcode.com/problems/add-digits/submissions/
+
+// Runtime: 25 ms, faster than 86.88% of C# online submissions for Add Digits.
+// Memory Usage: 26.7 MB, less than 9.05% of C# online submissions for Add Digits.
+
+public class Solution
+{
+    public int AddDigits(int num)
+    {
+        int result = num;
+        while (result >= 10)
+        {
+            num = result;
+            result = 0;
+            while (num > 0)
+            {
+                result += num % 10;
+                num /= 10;
+            }
+        }
+        return result;
+    }
+
+    public static void Main()
+    {
+        Solution x = new();
+        Console.WriteLine(x.AddDigits(38));
+    }
+}
